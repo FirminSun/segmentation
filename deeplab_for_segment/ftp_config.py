d89d29dcfe278772305b21ce79076929e544ff94
chou <zhoubin@0123@outlook.com>
2019-04-02 18:33:44 +0800

    add download data function test email send function successed

--- deeplab_for_segment/ftp_config.py
+++ deeplab_for_segment/ftp_config.py
@@ -10,12 +10,6 @@ host = "192.168.1.179"
 port = 21  # not str
 username = "zhb"
 password = "zjai8686"
-<<<<<<< HEAD
 # remote和local是相对客户端的
 ftp_home = ""
-=======
-
-# remote和local是相对客户端的
-ftp_home = "/data/segmentation_data/test_result"
->>>>>>> a9edff3cf27d4816c1c4159c5b10b70e16eed107
 localDir = ""
--- deeplab_for_segment/send_email.py
+++ deeplab_for_segment/send_email.py
@@ -9,32 +9,20 @@ from email.mime.text import MIMEText
 import time
 
 def notify(content):
-<<<<<<< HEAD
     msg_from = '1640968638@qq.com'  # 发送方邮箱
     passwd = 'rngdvajabbndbbcj'  # 填入发送方邮箱的授权码
     # msg_to = '931103972@qq.com, 841861601@qq.com,2092089369@qq.com'  # 收件人邮箱
     msg_to = ['1640968638@qq.com']#['Firmin.Sun@outlook.com','jiangxiaobaix@outlook.com', ] # 收件人邮箱
-=======
-    msg_from = 'choubin@outlook.com'  # 发送方邮箱
-    passwd = 'nrpsrzgwsmssbahf'  # 填入发送方邮箱的授权码
-    # msg_to = '931103972@qq.com, 841861601@qq.com,2092089369@qq.com'  # 收件人邮箱
-    msg_to = ['Firmin.Sun@outlook.com','jiangxiaobaix@outlook.com', 'jaykky.Lu@outlook.com'] # 收件人邮箱
->>>>>>> a9edff3cf27d4816c1c4159c5b10b70e16eed107
 
     subject = "segmentation result sent to tfp server"  # 主题
     msg = MIMEText(content)
     msg['Subject'] = subject
     msg['From'] = msg_from
     # msg['To'] = msg_to
-<<<<<<< HEAD
     
     try:
         s = smtplib.SMTP_SSL("smtp.qq.com", 465)
         s.connect("smtp.qq.com")
-=======
-    try:
-        s = smtplib.SMTP_SSL("smtp.qq.com", 465)
->>>>>>> a9edff3cf27d4816c1c4159c5b10b70e16eed107
         s.login(msg_from, passwd)
         s.sendmail(msg_from, msg_to, msg.as_string())
         print('Send email successful')
